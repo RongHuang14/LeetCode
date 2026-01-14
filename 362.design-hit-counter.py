55fdd6539ba0d04670c2f4e21162e42f7b8e2cfe
RongHuang14 <reginahuang.me@gmail.com>
2026-01-13 23:07:49 -0500

    1/13

---
+++ 362.design-hit-counter.py
@@ -0,0 +1,25 @@
+#
+# @lc app=leetcode id=362 lang=python3
+#
+# [362] Design Hit Counter
+#
+
+# @lc code=start
+class HitCounter:
+
+    def __init__(self):
+        
+
+    def hit(self, timestamp: int) -> None:
+        
+
+    def getHits(self, timestamp: int) -> int:
+        
+
+
+# Your HitCounter object will be instantiated and called as such:
+# obj = HitCounter()
+# obj.hit(timestamp)
+# param_2 = obj.getHits(timestamp)
+# @lc code=end
+
